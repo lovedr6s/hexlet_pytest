3cf72a4899544a580a58190ac641891057a0c4c1
lovedr6s <praense@icloud.com>
2024-10-15 16:26:43 +0300

    Added test_stacks

--- hexlet_pytest/example.py
+++ hexlet_pytest/example.py
@@ -1,2 +1,9 @@
 def reverse(string):
     return string[::-1]
+
+def stacks():
+    stack = []
+    stack.append(1)
+    stack.append(2)
+    stack.append(3)
+    return stack
--- tests/test_example.py
+++ tests/test_example.py
@@ -1,7 +1,13 @@
-from hexlet_pytest.example import reverse
+from hexlet_pytest.example import reverse, stacks
 
 def test_reverse():
     assert reverse('Hexlet') == 'telxeH'
 
 def test_reverse_for_empty_string():
     assert reverse('') == ''
+
+
+def test_stacks():
+    stack = stacks()
+    assert stack.pop() == 3
+    assert stack.pop() == 2
